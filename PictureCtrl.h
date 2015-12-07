89d5a227fd29045cd8411370426c2828db6c4b99
krishna <krishnaprasad.matta@gmail.com>
2015-12-06 21:16:10 -0600

    missed header file

---
+++ PictureCtrl.h
@@ -0,0 +1,25 @@
+#pragma once
+
+
+// CPictureCtrl
+
+class CPictureCtrl : public CStatic
+{
+	DECLARE_DYNAMIC(CPictureCtrl)
+
+public:
+	CPictureCtrl();
+	virtual ~CPictureCtrl();
+
+	//Loads an image from a file
+	BOOL LoadFromFile(CString &szFilePath);
+
+
+protected:
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnContextMenu(CWnd* /*pWnd*/, CPoint /*point*/);
+	afx_msg void OnRButtonDown(UINT nFlags, CPoint point);
+};
+
+
