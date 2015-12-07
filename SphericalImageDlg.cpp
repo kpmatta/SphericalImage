a4cb68d12e2c9ffc9fde3e5e12a1f5e5dcb4a1a5
krishna <krishnaprasad.matta@gmail.com>
2015-12-06 19:31:56 -0600

    SphericalImage First check in
    SphericalImage First check in

---
+++ SphericalImageDlg.cpp
@@ -0,0 +1,242 @@
+
+// SphericalImageDlg.cpp : implementation file
+//
+
+#include "stdafx.h"
+#include "SphericalImage.h"
+#include "SphericalImageDlg.h"
+#include "afxdialogex.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CAboutDlg dialog used for App About
+
+class CAboutDlg : public CDialogEx
+{
+public:
+	CAboutDlg();
+
+// Dialog Data
+	enum { IDD = IDD_ABOUTBOX };
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
+
+// Implementation
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+CAboutDlg::CAboutDlg() : CDialogEx(CAboutDlg::IDD)
+{
+}
+
+void CAboutDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CAboutDlg, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// CSphericalImageDlg dialog
+
+
+
+CSphericalImageDlg::CSphericalImageDlg(CWnd* pParent /*=NULL*/)
+	: CDialogEx(CSphericalImageDlg::IDD, pParent)
+{
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+}
+
+void CSphericalImageDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+	DDX_Control(pDX, IDC_STATIC_FACE_1, m_face_1);
+}
+
+BEGIN_MESSAGE_MAP(CSphericalImageDlg, CDialogEx)
+	ON_WM_SYSCOMMAND()
+	ON_WM_PAINT()
+	ON_WM_QUERYDRAGICON()
+	ON_STN_CLICKED(IDC_STATIC_FACE_1, &CSphericalImageDlg::OnStnClickedStaticFace1)
+	ON_STN_DBLCLK(IDC_STATIC_FACE_1, &CSphericalImageDlg::OnStnDblclickStaticFace1)
+	ON_BN_CLICKED(IDC_BUTTON1, &CSphericalImageDlg::OnBnClickedButton1)
+	ON_BN_CLICKED(IDOK, &CSphericalImageDlg::OnBnClickedOk)
+END_MESSAGE_MAP()
+
+
+// CSphericalImageDlg message handlers
+
+BOOL CSphericalImageDlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	// Add "About..." menu item to system menu.
+
+	// IDM_ABOUTBOX must be in the system command range.
+	ASSERT((IDM_ABOUTBOX & 0xFFF0) == IDM_ABOUTBOX);
+	ASSERT(IDM_ABOUTBOX < 0xF000);
+
+	CMenu* pSysMenu = GetSystemMenu(FALSE);
+	if (pSysMenu != NULL)
+	{
+		BOOL bNameValid;
+		CString strAboutMenu;
+		bNameValid = strAboutMenu.LoadString(IDS_ABOUTBOX);
+		ASSERT(bNameValid);
+		if (!strAboutMenu.IsEmpty())
+		{
+			pSysMenu->AppendMenu(MF_SEPARATOR);
+			pSysMenu->AppendMenu(MF_STRING, IDM_ABOUTBOX, strAboutMenu);
+		}
+	}
+
+	// Set the icon for this dialog.  The framework does this automatically
+	//  when the application's main window is not a dialog
+	SetIcon(m_hIcon, TRUE);			// Set big icon
+	SetIcon(m_hIcon, FALSE);		// Set small icon
+
+	// TODO: Add extra initialization here
+
+	return TRUE;  // return TRUE  unless you set the focus to a control
+}
+
+void CSphericalImageDlg::OnSysCommand(UINT nID, LPARAM lParam)
+{
+	if ((nID & 0xFFF0) == IDM_ABOUTBOX)
+	{
+		CAboutDlg dlgAbout;
+		dlgAbout.DoModal();
+	}
+	else
+	{
+		CDialogEx::OnSysCommand(nID, lParam);
+	}
+}
+
+// If you add a minimize button to your dialog, you will need the code below
+//  to draw the icon.  For MFC applications using the document/view model,
+//  this is automatically done for you by the framework.
+
+void CSphericalImageDlg::OnPaint()
+{
+	if (IsIconic())
+	{
+		CPaintDC dc(this); // device context for painting
+
+		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+
+		// Center icon in client rectangle
+		int cxIcon = GetSystemMetrics(SM_CXICON);
+		int cyIcon = GetSystemMetrics(SM_CYICON);
+		CRect rect;
+		GetClientRect(&rect);
+		int x = (rect.Width() - cxIcon + 1) / 2;
+		int y = (rect.Height() - cyIcon + 1) / 2;
+
+		// Draw the icon
+		dc.DrawIcon(x, y, m_hIcon);
+	}
+	else
+	{
+		m_face_1.UpdateWindow();
+
+		CDialogEx::OnPaint();
+	}
+}
+
+// The system calls this function to obtain the cursor to display while the user drags
+//  the minimized window.
+HCURSOR CSphericalImageDlg::OnQueryDragIcon()
+{
+	return static_cast<HCURSOR>(m_hIcon);
+}
+
+
+
+void CSphericalImageDlg::OnStnClickedStaticFace1()
+{
+	CImage img1;
+	int dimx = 100, dimy = 100;
+	img1.Load(_T("D:\\Krishna\\roses.jpg"));
+	//filename = path on local system to the bitmap
+
+	CDC *screenDC = GetDC();
+	CDC mDC;
+	mDC.CreateCompatibleDC(screenDC);
+	CBitmap b;
+	b.CreateCompatibleBitmap(screenDC, dimx, dimy);
+
+	CBitmap *pob = mDC.SelectObject(&b);
+	mDC.SetStretchBltMode(HALFTONE);
+	img1.StretchBlt(mDC.m_hDC, 0, 0, dimx, dimy, 0, 0, img1.GetWidth(), img1.GetHeight(), SRCCOPY);
+	mDC.SelectObject(pob);
+
+	
+
+	m_face_1.SetBitmap((HBITMAP)b.Detach());
+	//m_pictureCtrl.SetBitmap((HBITMAP)b.Detach());
+	ReleaseDC(screenDC);
+}
+
+
+void CSphericalImageDlg::OnStnDblclickStaticFace1()
+{
+	CImage img1;
+	int dimx = 100, dimy = 100;
+	img1.Load(_T("D:\\Krishna\\roses.jpg"));
+	//filename = path on local system to the bitmap
+
+	CDC *screenDC = GetDC();
+	CDC mDC;
+	mDC.CreateCompatibleDC(screenDC);
+	CBitmap b;
+	b.CreateCompatibleBitmap(screenDC, dimx, dimy);
+
+	CBitmap *pob = mDC.SelectObject(&b);
+	mDC.SetStretchBltMode(HALFTONE);
+	img1.StretchBlt(mDC.m_hDC, 0, 0, dimx, dimy, 0, 0, img1.GetWidth(), img1.GetHeight(), SRCCOPY);
+	mDC.SelectObject(pob);
+
+
+
+	m_face_1.SetBitmap((HBITMAP)b.Detach());
+	//m_pictureCtrl.SetBitmap((HBITMAP)b.Detach());
+	ReleaseDC(screenDC);
+}
+
+void CSphericalImageDlg::OnBnClickedButton1()
+{
+	CImage img1;
+	int dimx = 340, dimy = 397;
+	img1.Load(_T("D:\\Krishna\\roses.jpg"));
+	//filename = path on local system to the bitmap
+
+	CDC *screenDC = GetDC();
+	CDC mDC;
+	mDC.CreateCompatibleDC(screenDC);
+	CBitmap b;
+	b.CreateCompatibleBitmap(screenDC, dimx, dimy);
+
+	CBitmap *pob = mDC.SelectObject(&b);
+	mDC.SetStretchBltMode(HALFTONE);
+	img1.StretchBlt(mDC.m_hDC, 0, 0, dimx, dimy, 0, 0, img1.GetWidth(), img1.GetHeight(), SRCCOPY);
+	mDC.SelectObject(pob);
+
+	//m_face_1.ModifyStyle(0xF, SS_BITMAP, SWP_NOSIZE);
+	m_face_1.SetBitmap((HBITMAP)b.Detach());
+	//m_pictureCtrl.SetBitmap((HBITMAP)b.Detach());
+	ReleaseDC(screenDC);
+}
+
+
+void CSphericalImageDlg::OnBnClickedOk()
+{
+	// TODO: Add your control notification handler code here
+	CDialogEx::OnOK();
+}
---
+++ SphericalImageDlg.h
@@ -0,0 +1,39 @@
+
+// SphericalImageDlg.h : header file
+//
+
+#pragma once
+#include "afxwin.h"
+
+
+// CSphericalImageDlg dialog
+class CSphericalImageDlg : public CDialogEx
+{
+// Construction
+public:
+	CSphericalImageDlg(CWnd* pParent = NULL);	// standard constructor
+
+// Dialog Data
+	enum { IDD = IDD_SPHERICALIMAGE_DIALOG };
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+
+
+// Implementation
+protected:
+	HICON m_hIcon;
+
+	// Generated message map functions
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnStnClickedStaticFace1();
+	afx_msg void OnStnDblclickStaticFace1();
+	afx_msg void OnBnClickedButton1();
+	CStatic m_face_1;
+	afx_msg void OnBnClickedOk();
+};
